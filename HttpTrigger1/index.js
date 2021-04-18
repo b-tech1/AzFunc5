056a7869fbea31993ac3e5bc8e1554072aad17a1
Bartman <bwas@tripadvisor.com>
2021-04-18 21:19:04 +0100

    .

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,13 +1,15 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    if (req.body.pages[0].title){
+        context.res = {
+            body: "Page is " + req.body.pages[0].title + ", Action is " + req.body.pages[0].action + ", Event Type is " + req.headers['x-github-event']
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: ("Invalid payload for Wiki event")
+        }
+    }
 }
